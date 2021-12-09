a5f4df91f3ce288fa453a23e2642f01286394ce0
Ravindharreddy <94173176+Ravindharreddy@users.noreply.github.com>
2021-12-09 13:45:22 +0530

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,18 @@
+package com.technoelevate.mock;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Tree {
+	public static void main(String[] args) {
+		Set<String> set=new TreeSet<String>();
+		set.add("Sachin");
+		set.add("Dravid");
+		set.add("Dhoni");
+		set.add("Virat");
+		set.add("Rohit");
+		System.out.println(set);
+		
+	}
+
+}
